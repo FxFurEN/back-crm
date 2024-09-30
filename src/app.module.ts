57e75ee2addd67576ac47d545835c6d8dc956e7b
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-01 02:23:10 +0300

    redis service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './redis/redis.module';
+import { RedisService } from './redis/redis.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
@@ -12,8 +14,9 @@ import { UsersService } from './users/users.service';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    RedisModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RedisService],
 })
 export class AppModule {}
