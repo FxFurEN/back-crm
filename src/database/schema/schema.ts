2a5a4a7ea6990c757a955574d5f264468a79c5b7
FxFurEN <nikitostimoshenko@gmail.com>
2024-10-03 19:01:49 +0300

    add tasks and categories

--- src/database/schema/schema.ts
+++ src/database/schema/schema.ts
@@ -1,5 +1,5 @@
 import { createId } from '@paralleldrive/cuid2';
-import { pgEnum, pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import { numeric, pgEnum, pgTable, text, varchar } from 'drizzle-orm/pg-core';
 
 export const userRoleEnum = pgEnum('user_role', ['ADMIN', 'USER']);
 
@@ -10,4 +10,18 @@ export const users = pgTable('users', {
   refreshToken: text('refresh_token').default(null),
   password: text('password'),
   role: userRoleEnum('role').default('USER'),
+});
+
+export const tasks = pgTable('tasks', {
+  id: varchar('id', { length: 256 }).primaryKey().$defaultFn(createId),
+  name: text('name'),
+  categoryId: varchar('category_id', { length: 256 }).references(
+    () => categories.id,
+  ),
+  price: numeric('price', { precision: 10, scale: 2 }),
+});
+
+export const categories = pgTable('categories', {
+  id: varchar('id', { length: 256 }).primaryKey().$defaultFn(createId),
+  name: text('name').unique(),
 });
